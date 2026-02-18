8021f409b704f8d72b7e1cbdb973a40442114902
Bikz <Bikrambrar25@gmail.com>
2026-02-17 21:21:58 -0500

    epic8: expand design tokens for UI mods

--- packages/CodexChatUI/Sources/CodexChatUI/DesignTokens.swift
+++ packages/CodexChatUI/Sources/CodexChatUI/DesignTokens.swift
@@ -60,33 +60,170 @@ public struct DesignTokens: Hashable, Sendable {
     public var spacing: Spacing
     public var radius: Radius
     public var palette: Palette
+    public var materials: Materials
+    public var bubbles: Bubbles
+    public var iconography: Iconography
 
     public init(
         typography: Typography = Typography(),
         spacing: Spacing = Spacing(),
         radius: Radius = Radius(),
-        palette: Palette = Palette()
+        palette: Palette = Palette(),
+        materials: Materials = Materials(),
+        bubbles: Bubbles = Bubbles(),
+        iconography: Iconography = Iconography()
     ) {
         self.typography = typography
         self.spacing = spacing
         self.radius = radius
         self.palette = palette
+        self.materials = materials
+        self.bubbles = bubbles
+        self.iconography = iconography
     }
 
     public static let `default` = DesignTokens()
 
     public func applying(override: ModThemeOverride) -> DesignTokens {
         var copy = self
-        if let accentHex = override.accentHex {
+        if let titleSize = override.typography?.titleSize {
+            copy.typography.titleSize = CGFloat(titleSize)
+        }
+        if let bodySize = override.typography?.bodySize {
+            copy.typography.bodySize = CGFloat(bodySize)
+        }
+        if let captionSize = override.typography?.captionSize {
+            copy.typography.captionSize = CGFloat(captionSize)
+        }
+
+        if let xSmall = override.spacing?.xSmall {
+            copy.spacing.xSmall = CGFloat(xSmall)
+        }
+        if let small = override.spacing?.small {
+            copy.spacing.small = CGFloat(small)
+        }
+        if let medium = override.spacing?.medium {
+            copy.spacing.medium = CGFloat(medium)
+        }
+        if let large = override.spacing?.large {
+            copy.spacing.large = CGFloat(large)
+        }
+
+        if let small = override.radius?.small {
+            copy.radius.small = CGFloat(small)
+        }
+        if let medium = override.radius?.medium {
+            copy.radius.medium = CGFloat(medium)
+        }
+        if let large = override.radius?.large {
+            copy.radius.large = CGFloat(large)
+        }
+
+        if let accentHex = override.resolvedPaletteAccentHex {
             copy.palette.accentHex = accentHex
         }
-        if let backgroundHex = override.backgroundHex {
+        if let backgroundHex = override.resolvedPaletteBackgroundHex {
             copy.palette.backgroundHex = backgroundHex
         }
-        if let panelHex = override.panelHex {
+        if let panelHex = override.resolvedPalettePanelHex {
             copy.palette.panelHex = panelHex
         }
+
+        if let panel = override.materials?.panelMaterial,
+           let material = DesignMaterial(rawValue: panel) {
+            copy.materials.panelMaterial = material
+        }
+        if let card = override.materials?.cardMaterial,
+           let material = DesignMaterial(rawValue: card) {
+            copy.materials.cardMaterial = material
+        }
+
+        if let style = override.bubbles?.style,
+           let bubbleStyle = BubbleStyle(rawValue: style) {
+            copy.bubbles.style = bubbleStyle
+        }
+        if let userHex = override.bubbles?.userBackgroundHex {
+            copy.bubbles.userBackgroundHex = userHex
+        }
+        if let assistantHex = override.bubbles?.assistantBackgroundHex {
+            copy.bubbles.assistantBackgroundHex = assistantHex
+        }
+
+        if let iconStyle = override.iconography?.style,
+           let resolved = Iconography.Style(rawValue: iconStyle) {
+            copy.iconography.style = resolved
+        }
+
         return copy
+    }
+}
+
+public extension DesignTokens {
+    enum DesignMaterial: String, CaseIterable, Hashable, Sendable, Codable {
+        case ultraThin = "ultraThin"
+        case thin = "thin"
+        case regular = "regular"
+        case thick = "thick"
+        case ultraThick = "ultraThick"
+
+        public var material: Material {
+            switch self {
+            case .ultraThin:
+                return .ultraThinMaterial
+            case .thin:
+                return .thinMaterial
+            case .regular:
+                return .regularMaterial
+            case .thick:
+                return .thickMaterial
+            case .ultraThick:
+                return .ultraThickMaterial
+            }
+        }
+    }
+
+    struct Materials: Hashable, Sendable {
+        public var panelMaterial: DesignMaterial
+        public var cardMaterial: DesignMaterial
+
+        public init(panelMaterial: DesignMaterial = .thin, cardMaterial: DesignMaterial = .regular) {
+            self.panelMaterial = panelMaterial
+            self.cardMaterial = cardMaterial
+        }
+    }
+
+    enum BubbleStyle: String, CaseIterable, Hashable, Sendable, Codable {
+        case plain = "plain"
+        case glass = "glass"
+        case solid = "solid"
+    }
+
+    struct Bubbles: Hashable, Sendable {
+        public var style: BubbleStyle
+        public var userBackgroundHex: String
+        public var assistantBackgroundHex: String
+
+        public init(
+            style: BubbleStyle = .glass,
+            userBackgroundHex: String = "#2E7D32",
+            assistantBackgroundHex: String = "#FFFFFF"
+        ) {
+            self.style = style
+            self.userBackgroundHex = userBackgroundHex
+            self.assistantBackgroundHex = assistantBackgroundHex
+        }
+    }
+
+    struct Iconography: Hashable, Sendable {
+        public enum Style: String, CaseIterable, Hashable, Sendable, Codable {
+            case sfSymbols = "sf-symbols"
+        }
+
+        public var style: Style
+
+        public init(style: Style = .sfSymbols) {
+            self.style = style
+        }
     }
 }
 
